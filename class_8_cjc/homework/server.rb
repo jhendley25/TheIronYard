e19487843998d9e0cf0d910b132eede753aa522e
Christopher Cooper <cjcoopersc@gmail.com>
2014-04-14 00:48:17 -0400

    updating website

--- class_8_cjc/homework/server.rb
+++ class_8_cjc/homework/server.rb
@@ -1,6 +1,46 @@
 require 'sinatra'
 require 'faker'
 
-get '/' do 
-  erb :hello_world
+# get '/' do 
+#   erb :hello_world
+# end
+get '/' do
+  # get the index layout
+  # erb layout: :index
+@homepage_lorem_text = "Lorem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin Irem ipsum dolor some Latin I don't know."
+  erb :homepage, layout: :index
 end
+get '/jobs' do
+  @jobs = [ {job: "Developer"},
+            {job: "SystemsAdmin"},
+            {job: "Programmer"}, 
+            {job: "ServerTech"},
+            {job: "Engineer"}, 
+            {job: "Manager"},
+            {job: "Instructor"}, 
+            {job: "Trainer"},
+            {job: "NetworkTech"},
+            {job: "NetworkEngineer"},
+            {job: "VirtualEngineer"},
+          ]
+  # Do Jobs stuff here.
+   erb :render_jobs, layout: :index
+end
+
+get '/talent' do
+  @talent= [ {talent: "Developer"},
+            {talent: "SystemsAdmin"},
+            {talent: "Programmer"}, 
+            {talent: "ServerTech"},
+            {talent: "Engineer"}, 
+            {talent: "Manager"},
+            {talent: "Instructor"}, 
+            {talent: "Trainer"},
+            {talent: "NetworkTech"},
+            {talent: "NetworkEngineer"},
+            {talent: "VirtualEngineer"},
+          ]
+  # Do Jobs stuff here.
+   erb :render_talent, layout: :index
+end
+
