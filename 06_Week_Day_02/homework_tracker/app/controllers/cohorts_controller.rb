ef4b80517aa489d9230c97d442653fe93da231ca
Christopher Cooper <cjcoopersc@gmail.com>
2014-05-10 12:04:32 -0400

    cohorts controller

--- 06_Week_Day_02/homework_tracker/app/controllers/cohorts_controller.rb
+++ 06_Week_Day_02/homework_tracker/app/controllers/cohorts_controller.rb
@@ -1,2 +1,18 @@
 class CohortsController < ApplicationController
+  
+  def index
+  end
+
+  def new
+    @cohorts = @location.cohorts.new
+  end
+
+  def create
+    @cohorts = @location.cohorts.create cohorts_params
+    redirect_to location_path(@location)
+  end
+
+  def show
+    @cohorts = @location.cohorts cohorts_params
+  end
 end
