418e387226e0f86380ce33927082d00f9f7deec6
Christopher Cooper <cjcoopersc@gmail.com>
2014-05-14 16:52:31 -0400

    headache of bootstrap-gem

---
+++ 07_Week_Day_03/ActiveListApp/db/migrate/20140514190121_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :item_name
+      t.string :urgency_level
+      t.integer :location_id
+
+      t.timestamps
+    end
+  end
+end
